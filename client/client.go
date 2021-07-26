1674d894bec1daf8062b0637fc1376f80cde6c77
root <root@RK.localdomain>
2021-07-26 13:12:07 +0500

    updated server and client

--- client/client.go
+++ client/client.go
@@ -5,22 +5,30 @@ import (
 	"fmt"
 	"net"
 	"os"
-	"time"
 )
 
 func main() {
-	//if len(os.Args) < 2 {
-	//	fmt.Print("You must enter your name as argument\n")
-	//	os.Exit(1)
-	//}
+	// Exit with status 1 if name is not provided
+	if len(os.Args) < 2 {
+		fmt.Print("You must enter your name as argument\n")
+		os.Exit(1)
+	}
+	// server address and port
 	host := "127.0.0.1:8000"
+	// dial the server
 	conn, err := net.Dial("tcp", host)
+	// if connection is not successful, exit.
 	if err != nil {
 		fmt.Println("Cannot connect to server")
 		os.Exit(1)
 	}
-	//name := fmt.Sprintf("%s\n", os.Args[1])
-	//conn.Write([]byte(name))
+	// send the name of client, as provided in os args, to the server
+	name := fmt.Sprintf("%s\n", os.Args[1])
+	conn.Write([]byte(name))
+
+	// manage client requests
+
+	// go routine to read client data from its stdin and sending it to the server.
 	go func(conn net.Conn) {
 		r := bufio.NewReader(os.Stdin)
 		for {
@@ -29,12 +37,18 @@ func main() {
 		}
 	}(conn)
 
+	// go routine for receing data from server and displaying it to the client's terminal
 	go func(conn net.Conn) {
 		s := bufio.NewReader(conn)
 		for {
 			content, _ := s.ReadBytes('\n')
-			fmt.Printf("Client Message: %s", string(content))
+			fmt.Printf("%s", string(content))
+			//fmt.Printf("Client Message: %s", string(content))
 		}
 	}(conn)
-	time.Sleep(1 * time.Minute)
+
+	// run for infinite, until client closes the connection.
+	for {
+
+	}
 }
--- server/server.go
+++ server/server.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"net"
 	"os"
-	"time"
+	"strings"
 )
 
 type Client struct {
@@ -15,61 +15,87 @@ type Client struct {
 }
 
 func main() {
+	// array to store the address of client record
 	clients := make([]*Client, 2)
-	//conns := make([]net.Conn, 2)
+	// channel to know whether any client has been disconnected
+	connections := make(chan bool, 2)
+	// for the id of client
 	count := 0
+	// host address is "127.0.0.1 and port is "8000"
 	host := "127.0.0.1:8000"
+
+	// run the server
 	lis, err := net.Listen("tcp", host)
 	if err != nil {
 		fmt.Println("[server]: error connecting to tcp socket")
 		os.Exit(1)
 	}
+
 	fmt.Println("Server listening at port 8000")
-
+	// wait for the connection by a client
 	for {
-
 		conn, err := lis.Accept()
 		if err != nil {
 			fmt.Println("Cannot connect to the client")
 			continue
 		}
-		//time.Sleep(100 * time.Millisecond)
-		count++
+		// new client record
 		c := &Client{
 			name: "",
 			conn: conn,
 			id:   count,
 		}
 		fmt.Printf("Client %d is connected\n", count)
-		clients[count-1] = c
+		clients[count] = c
+		count++
+
 		if count == 2 {
 			for i := range clients {
-				go handleClient(clients[i], clients)
+				// handle each client in seperate go routine
+				go handleClient(clients[i], clients, connections)
 			}
 			break
 		}
 	}
-	time.Sleep(1 * time.Minute)
-}
-
-func handleClient(client *Client, clients []*Client) {
-	reader := bufio.NewReader(client.conn)
-	name, _ := reader.ReadBytes('\n')
-	client.name = string(name)
+	// wait for the connections channel to reach a size of 2 before exiting the main function
 	for {
-		data, _ := reader.ReadBytes('\n')
-		fmt.Printf("Client message: %s", string(data))
-		//fromS, _ := r.ReadBytes('\n')
-		broadcast(client, clients, data)
+		if len(connections) == 2 {
+			break
+		}
 	}
 }
 
-func broadcast(client *Client, clients []*Client, data []byte) {
-	message := "Message from " + client.name + ": " + string(data)
-	newMsg := []byte(message)
+func handleClient(client *Client, clients []*Client, connections chan bool) {
+	// bufio reader to read the content send by a client to server
+	reader := bufio.NewReader(client.conn)
+	// read the name of the client provided by the client itself after connection
+	name, _ := reader.ReadBytes('\n')
+	client.name = strings.TrimSuffix(string(name), "\n")
+
+	// serve data requests of client
+	for {
+		data, err := reader.ReadBytes('\n')
+		// client conn is closed. Exit the function
+		if err != nil {
+			msg := client.name + " is disconnected\n"
+			fmt.Println(msg)
+			connections <- false
+			broadcast(client, clients, msg)
+			break
+		}
+		// broadcast data to all clients
+		if len(data) >= 1 {
+			fmt.Printf("Client message: %s", string(data))
+			msg := "Message from " + client.name + ": " + string(data)
+			broadcast(client, clients, msg)
+		}
+	}
+}
+
+func broadcast(client *Client, clients []*Client, data string) {
 	for _, j := range clients {
-		if client.conn != j.conn {
-			(j.conn).Write(newMsg)
+		if client.id != j.id {
+			(j.conn).Write([]byte(data))
 		}
 	}
 }
